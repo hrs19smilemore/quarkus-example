caa8c4fb8e1670103c81e0055874451300868808
shubbard <Bar7Mad2$#%>
2022-09-30 13:03:38 -0300

    v2

--- src/main/java/com/example/controllers/CalculatorController.java
+++ src/main/java/com/example/controllers/CalculatorController.java
@@ -5,53 +5,37 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/calculator")
 public class CalculatorController {
-    int balance;
 
-    @Path("/balance")
+    @Path("/add/{firstValue}/{secondValue}")
     @GET
     @Produces(value = MediaType.TEXT_PLAIN)
-    public int getBalance(){
-        return this.balance;
+    @Consumes(value = MediaType.TEXT_PLAIN)
+    public int add(@PathParam("firstValue") int firstValue, @PathParam("secondValue") int secondValue){
+        return firstValue + secondValue;
     }
 
-    @Path("/set/{balance}")
-    @POST
-    @Produces(value = MediaType.APPLICATION_JSON)
-    @Consumes(value = MediaType.APPLICATION_JSON)
-    public void setBalance(@PathParam("balance") int balance){
-        this.balance = balance;
+    @Path("/minus/{firstValue}/{secondValue}")
+    @GET
+    @Produces(value = MediaType.TEXT_PLAIN)
+    @Consumes(value = MediaType.TEXT_PLAIN)
+    public int minus(@PathParam("firstValue") int firstValue, @PathParam("secondValue") int secondValue){
+        return firstValue - secondValue;
     }
 
-    @Path("/add/{value}")
-    @POST
-    @Produces(value = MediaType.APPLICATION_JSON)
-    @Consumes(value = MediaType.APPLICATION_JSON)
-    public void add(@PathParam("value") int value){
-        this.balance += value;
+    @Path("/divide/{firstValue}/{secondValue}")
+    @GET
+    @Produces(value = MediaType.TEXT_PLAIN)
+    @Consumes(value = MediaType.TEXT_PLAIN)
+    public int divide(@PathParam("firstValue") int firstValue, @PathParam("secondValue") int secondValue){
+        return firstValue / secondValue;
     }
 
-    @Path("/minus/{value}")
-    @POST
-    @Produces(value = MediaType.APPLICATION_JSON)
-    @Consumes(value = MediaType.APPLICATION_JSON)
-    public void minus(@PathParam("value") int value){
-        this.balance -= value;
-    }
-
-    @Path("/divide/{value}")
-    @POST
-    @Produces(value = MediaType.APPLICATION_JSON)
-    @Consumes(value = MediaType.APPLICATION_JSON)
-    public void divide(@PathParam("value") int value){
-        this.balance /= value;
-    }
-
-    @Path("/multiply/{value}")
-    @POST
-    @Produces(value = MediaType.APPLICATION_JSON)
-    @Consumes(value = MediaType.APPLICATION_JSON)
-    public void multiply(@PathParam("value") int value){
-        this.balance *= value;
+    @Path("/multiply/{firstValue}/{secondValue}")
+    @GET
+    @Produces(value = MediaType.TEXT_PLAIN)
+    @Consumes(value = MediaType.TEXT_PLAIN)
+    public int multiply(@PathParam("firstValue") int firstValue, @PathParam("secondValue") int secondValue){
+        return firstValue * secondValue;
     }
 
 }
